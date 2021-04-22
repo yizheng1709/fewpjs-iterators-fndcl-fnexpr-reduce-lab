9f5e66aa6084ca6e9482876e88d91bf5b026a8e7
Eva <yi.zheng1709@gmail.com>
2021-04-21 20:58:10 -0400

    commit tree ad516a04622d4f4c1c29ea839cf92b1321afc4ba

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function countBatteries(batch, total) {
+    return batch + total 
+}
+
+let totalBatteries = batteryBatches.reduce(countBatteries, 0)
